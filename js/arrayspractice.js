13fd146773aaa732ababe17fda17658edb6e9fd2
kapil-9672 <kapil.kumawat@dqotsolutions.com>
2022-10-18 14:51:01 +0530

    new learn

---
+++ js/arrayspractice.js
@@ -0,0 +1,47 @@
+console.log("Hello");
+
+// qustion 1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8,98];
+// let a = prompt("Enter a Number");
+
+// a = Number.parseInt(a);
+// arr.push(a)
+// console.log(arr);
+
+
+// qustion 2
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8,98];
+// let a;
+// do {
+//     a = prompt("Enter a Number");
+//     a = Number.parseInt(a)
+//     arr.push(a);
+// }while(a != 0);
+// console.log(arr);
+
+// qustion 3
+
+// let arr = [1, 2, 30, 4, 5, 6, 7, 8,98,30, 50,200];
+// let a = arr.filter((x)=>{
+//     return x%10 == 0
+// })
+// console.log(a);
+
+// qustion 4
+
+    // let arr = [1, 2, 30, 4, 5, 6, 7, 8,98,30, 50,200];
+    // let a = arr.map((x)=>{
+    //     return x*x
+    // })
+    // console.log(a);
+
+
+// qustion 5
+
+    // let arr = [1, 2, 3, 4, 5, 6, 7, 8,9];
+    // let a = arr.reduce((x1 , x2)=>{
+    //     return x1 * x2
+    // })
+    // console.log(a);
---
+++ js/randomnumber.js
@@ -0,0 +1,33 @@
+// let a = Math.random() * 10;
+
+// console.log(a);
+
+// let a = Math.floor(Math.random() * 101);
+
+// console.log(a);
+
+
+// let a = Math.floor(Math.random() * 10) + 1;
+
+// console.log(a);
+
+// let a = Math.floor(Math.random() * (25 - 15)) + 15;
+
+// console.log(a);
+
+
+
+// function getRandom(min, max){
+//     let a = Math.floor(Math.random() * (max - min)) + min;
+//     return a
+// }
+
+// console.log(getRandom(25, 35));
+
+
+// function getRandom(min, max){
+//     let a = Math.floor(Math.random() * (max - min + 1)) + min;
+//     return a
+// }
+
+// console.log(getRandom(25, 35));
