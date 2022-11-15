7239d7370c889b7a9b23d9138c55442151ddbc23
kapil-966 <kapilkumawat966@gmail.com>
2022-11-15 23:07:00 +0530

    new

---
+++ js/inner&outerHTML.js
@@ -0,0 +1,10 @@
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+
+
+let x = document.getElementsByTagName('span')[0];
+console.log(x)
+let y = document.getElementsByTagName('span')[0];
+console.dir(y);         
+
+// console.log(document.body.textContent);
