153c92ef99ce79e69d0f86c1688dbd60617fe9a8
kapil-966 <kapilkumawat966@gmail.com>
2022-11-16 22:54:54 +0530

    new

---
+++ js/setInterval.js
@@ -0,0 +1,19 @@
+// alert("Hello");
+
+// let a = setTimeout(function(){
+//     alert("Hey how are you")
+// },2000 );
+
+// console.log(a);
+// clearTimeout(a);
+
+// const sum = (a, b) =>{
+//     console.log("Yes i am Running " + (a + b))
+//     a + b
+// }
+// setTimeout(sum, 2000, 1, 2)
+
+// setInterval(sum, 1000, 1, 2, 7);
+// setInterval(function(){
+// alert("Aram Aram")
+// },5000 );
