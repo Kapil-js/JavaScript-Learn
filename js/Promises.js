23c84bb2079d21775c71dc6a9c7e32191e3c71ad
kapil-966 <kapilkumawat966@gmail.com>
2022-11-21 22:26:39 +0530

    new commit

--- js/Promises.js
+++ js/Promises.js
@@ -1,16 +1,48 @@
-console.log("Hello one");
+// console.log("Hello one");
 
-setTimeout(function(){
-    console.log("Hello Two in 2 seconds");
-}, 2000
-)
-console.log("My name is " + "Jhon");
+// setTimeout(function(){
+//     console.log("Hello Two in 2 seconds");
+// }, 2000
+// )
+// console.log("My name is " + "Jhon");
 
 
 
-let promise = new Promise(function(resolve, reject){
-    alert("Hello")
-    resolve(56)
-});
+// let promise = new Promise(function(resolve, reject){
+//     alert("Hello")
+//     resolve(56)
+// });
 
-console.log(promise);
+// console.log(promise);   
+
+
+let p = new Promise((reslove, reject)=>{
+    console.log("promise is pending" );
+    setTimeout(()=>{
+        console.log("I am promise and I am reslove")
+        // reslove(true)
+        reslove("I am an reslove")
+    },5000)
+})
+
+console.log(p);
+
+let p2 = new Promise((reslove, reject)=>{
+    console.log("promise is pending" );
+    setTimeout(()=>{
+        console.log("I am promise and I am Fullfilled")
+        // reslove(true)
+        reject(new Error("I am an error"))
+    },5000)
+})
+
+console.log(p2);
+
+// catch value
+p.then((value) => {
+    console.log(value)
+})
+// catch error
+p2.then((error) =>{
+    console.log("some error")   
+})
